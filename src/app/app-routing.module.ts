055a8220e02b051456212b3509d44d332565f925
Franz Wilder <gualambo@gmail.com>
2023-11-19 23:47:36 -0500

    used basic Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BrandPagesComponent } from './brand/pages/brand-pages/brand-pages.component';
+import { DesignPageComponent } from './design/pages/design-page/design-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'brand',
+    component: BrandPagesComponent
+  },
+  {
+    path: 'design',
+    component: DesignPageComponent
+  },
+  {
+    path: '**',
+    component:BrandPagesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
