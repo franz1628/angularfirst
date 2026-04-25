7461cbf734edc60e8b109523d8ec32502d01aaaa
franz1628 <franz1628@users.noreply.github.com>
2026-04-25 22:30:11 +0000

    deploy: 2f761273883e849b7b12fee1b64387ba7289d224

--- chunk-B5NPWBBO.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-Z5XO4WT2.js";import"./chunk-PANGYDKF.js";export{a as BrandModule};
---
+++ chunk-VDQ6KAIH.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-DAGMJX52.js";import"./chunk-DENVTWGC.js";export{a as BrandModule};
